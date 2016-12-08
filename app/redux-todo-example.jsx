98c35545ff8d6c2ddd65a2a024435cb0e0b1ce95
mwygoda <mwygoda_94@o2.pl>
2016-12-09 00:25:06 +0100

    backup

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -1,19 +1,47 @@
 var redux = require('redux');
 
-console.log('Starting redux example');
+console.log('Starting todo redux example');
 
-var stateDefault = {searchText: '',
-showCompleted: false,
-todo: {}
+var stateDefault = {
+  searchText: '',
+  showCompleted: false,
+  todos: []
 };
-
 var reducer = (state = stateDefault, action) => {
-  // state = state || {name: 'Anonymous'};
-
-  return state;
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      };
+    default:
+      return state;
+  }
 };
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
-var store = redux.createStore(reducer);
+// Subscribe to changes
+store.subscribe(() => {
+  var state = store.getState();
 
-var currentState = store.getState();
-console.log('currentState ', currentState);
+  document.getElementById('app').innerHTML = state.searchText;
+});
+
+console.log('currentState', store.getState());
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'work'
+});
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'dog'
+});
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'Something else'
+});
